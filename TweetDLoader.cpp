454f432a7cabf3df3f01cdeced91858907d45f04
Agustin Gullino <agullino@itba.edu.ar>
2021-05-09 18:24:28 -0300

    Agregados

--- TweetDLoader.cpp
+++ TweetDLoader.cpp
@@ -6,9 +6,12 @@ using json = nlohmann::json;
 static size_t recieveCallback(char* ptr, size_t size, size_t nmemb, void* userdata);
 
 
-TweetDLoader::TweetDLoader(const char* API_key, const char* API_SecretKey) : APIKey(API_key), 
-																			APISecretKey(API_SecretKey)
+TweetDLoader::TweetDLoader(const char* API_key, const char* API_SecretKey) :
+	APIKey(API_key), 
+	APISecretKey(API_SecretKey),
+	queryLink("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={username}&count={tweetcount}")
 {
+	
 	this->curl = curl_easy_init();
 	if (curl || API_key == nullptr || API_SecretKey == nullptr)
 	{
@@ -31,7 +34,6 @@ TweetDLoader::TweetDLoader(const char* API_key, const char* API_SecretKey) : API
 
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, receivedData);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &this->receivedData);
-
 		if (curl_easy_perform(curl) != CURLE_OK)
 		{
 			curl_easy_cleanup(curl);
@@ -43,19 +45,94 @@ TweetDLoader::TweetDLoader(const char* API_key, const char* API_SecretKey) : API
 
 		json recievedJson = json::parse(this->receivedData);
 		this->token = recievedJson["access_token"];
+		
+		this->status = IDLE;
 
 	}
 	else
 	{
 		throw std::exception("ERR: Unable to initialize cURL");
 	}
+	
+}
+
+int TweetDLoader::getStatus()
+{
+	return this->status;
 }
 
 
 bool TweetDLoader::download(std::list<std::string>& buffer, const char* usrname, unsigned int tweetCount)
 {
+	if (this->status == IDLE)
+	{
 
+		std::string usrnameTag = "{username}";
+		std::string tweetcountTag = "{tweetcount}";
 
+		if (tweetCount != 0)
+		{
+			this->queryLink.replace(queryLink.find(usrnameTag), usrnameTag.length(), usrname);
+			this->queryLink.replace(queryLink.find(tweetcountTag), tweetcountTag.length(), std::to_string(tweetCount));
+		}
+		else
+		{
+			this->queryLink.replace(queryLink.find(usrnameTag), usrnameTag.length(), usrname);
+			this->queryLink.replace(queryLink.find(tweetcountTag), tweetcountTag.length(), std::to_string(DEFAULT_TWEET_COUNT));
+		}
+
+		this->curl = curl_easy_init();
+		this->multiCurl = curl_multi_init();
+		this->receivedData.clear();
+
+		if (this->curl && this->multiCurl)
+		{
+
+			curl_multi_add_handle(this->multiCurl, this->curl);
+
+			curl_easy_setopt(curl, CURLOPT_URL, this->queryLink.c_str());
+			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+			curl_easy_setopt(curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
+
+			struct curl_slist* list = nullptr;
+			std::string aux = "Authorization: Bearer ";
+			aux = aux + token;
+			list = curl_slist_append(list, aux.c_str());
+			curl_easy_setopt(this->curl, CURLOPT_HTTPHEADER, list);
+
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, receivedData);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &this->receivedData);
+
+			curl_multi_perform(this->multiCurl, &this->status);
+
+		}
+		else
+		{
+			throw std::exception("ERR: Unable to initialize cURL");
+		}
+	}
+	else
+	{
+		this->err = curl_multi_perform(this->multiCurl, &this->status);
+		if (this->status == IDLE)
+		{
+			if (this->err != CURLM_OK)
+			{
+				curl_easy_cleanup(this->curl);
+				curl_multi_cleanup(this->multiCurl);
+				throw std::exception(curl_multi_strerror(this->err));
+			}
+			else
+			{
+				curl_easy_cleanup(this->curl);
+				curl_multi_cleanup(this->multiCurl);
+
+				//TODO: Agregar parseo a lista
+				return 0;
+			}
+		}
+	}
+	return 1;
 
 }
 
--- TweetDLoader.h
+++ TweetDLoader.h
@@ -3,20 +3,28 @@
 #include <curl/curl.h>
 #include <list>
 
+#define DEFAULT_TWEET_COUNT 1
+
+typedef enum {IDLE = 0, DOWNLOADING} state_t;
+
 class TweetDLoader
 {
 public:
 	TweetDLoader(const char* API_key, const char* API_SecretKey);
 
+	int getStatus();
 	bool download(std::list<std::string>& buffer, const char* usrname, unsigned int tweetCount = 0);
 
 private:
+	int status;
 	const char* APIKey;
 	const char* APISecretKey;
 	std::string token;
 	std::string receivedData;
+	std::string queryLink;
 	CURL* curl;
 	CURLM* multiCurl;
+	CURLMcode err;
 
 	
 };
